9f03b40de6a0f899ca56f5798923a2cfdd8271e2
gayeon <ini03134@naver.com>
2024-02-15 17:42:20 +0900

    쿠키활용

---
+++ 2. web-nod/9. 쿠키/popup.js
@@ -0,0 +1,23 @@
+
+
+
+function createCookie(name) {
+  const date = new Date();
+  date.setDate(date.getDate() + 1); //쿠카의 수명을 하루로
+  const cookie = `${name}=true;expires=${date.toUTCString()}`;  //중간에 세미콜론 꼭찍기
+
+  //쿠키생성
+  document.cookie = cookie;
+}
+
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+
+  for(let c of cookies) {
+    if(c.search(name) !== -1){
+      return true;
+    }
+  }
+
+  return false;
+}
