71dfc33c584cfc5be9d7143ce82b6c149ea5ceb8
gayeon <ini03134@naver.com>
2024-02-16 14:24:10 +0900

    calculator완성

--- 2. web-nod/App/assets/scripts/vendor.js
+++ 2. web-nod/App/assets/scripts/vendor.js
@@ -1,1 +1,15 @@
-//취득한요소
+//취득한 요소
+
+// 프로그램에서 전역적으로 사용할 변수,함수를 정의
+const $userInput = document.getElementById('input-number');
+const $addBtn = document.getElementById('btn-add');
+const $subtractBtn = document.getElementById('btn-subtract');
+const $multiplyBtn = document.getElementById('btn-multiply');
+const $divideBtn = document.getElementById('btn-divide');
+
+
+const $currentCalculationOutput = document.getElementById('current-calculation');
+const $currentResultOutput = document.getElementById('current-result');
+
+const $logEntries = document.querySelector('.log-entries');
+
